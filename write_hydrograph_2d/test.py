6ecffb37efa2c8b9d4a1e980daf2551c98c7deca
namedyangfan <cedric.yf@gmail.com>
2017-07-04 14:29:50 -0400

    make test
    make test

--- write_hydrograph_2d/test.py
+++ write_hydrograph_2d/test.py
@@ -1,22 +1,8 @@
 from write_hydrograph_2dlist import write_hydrograph_2d
 
-input_folder = r"D:\ARB\Mesh Generation_AlgoMesh Tutorial\Mesh_Steve_Jun_2017\hydrograph\AssnSubBsn" 
-output_folder = r"D:\ARB\Mesh Generation_AlgoMesh Tutorial\Mesh_Steve_Jun_2017\hydrograph\hgs_command"
-output_name = "AssnSubBsn.dat"
-input_folder_rel_path = "./node_lists"
-
-write_hydrograph_2d(input_folder, input_folder_rel_path, output_folder, output_name, ldebug=True)
-
-input_folder = r"D:\ARB\Mesh Generation_AlgoMesh Tutorial\Mesh_Steve_Jun_2017\hydrograph\QuApp" 
-output_folder = r"D:\ARB\Mesh Generation_AlgoMesh Tutorial\Mesh_Steve_Jun_2017\hydrograph\hgs_command"
-output_name = "Qu_appelle.dat"
-input_folder_rel_path = "./node_lists"
-
-write_hydrograph_2d(input_folder, input_folder_rel_path, output_folder, output_name, ldebug=True)
-
-input_folder = r"D:\ARB\Mesh Generation_AlgoMesh Tutorial\Mesh_Steve_Jun_2017\hydrograph\Sour" 
-output_folder = r"D:\ARB\Mesh Generation_AlgoMesh Tutorial\Mesh_Steve_Jun_2017\hydrograph\hgs_command"
-output_name = "Souris.dat"
-input_folder_rel_path = "./node_lists"
+input_folder = r"./test" 
+output_folder = r"./test"
+output_name = "test.dat"
+input_folder_rel_path = "./test"
 
 write_hydrograph_2d(input_folder, input_folder_rel_path, output_folder, output_name, ldebug=True)
